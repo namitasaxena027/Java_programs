a742830465e5a8658bce275584c2d42543bca01b
Namita Saxena <namitasaxena@samirs-air.lan>
2023-02-24 22:44:04 -0800

    Add Marsweek

---
+++ MarsWeek/src/GuessingGame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hi, what's your name?");
+        String name = s.nextLine();
+    }
+
+
+    
+}

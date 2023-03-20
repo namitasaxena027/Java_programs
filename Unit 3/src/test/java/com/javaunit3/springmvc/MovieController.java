857d129dfea80e312a0a4c6857689b8c96dd6e64
namitasaxena027 <raizadan8@gmail.com>
2023-03-20 15:26:05 -0700

    Unit 3 project 4

--- Unit 3/src/test/java/com/javaunit3/springmvc/MovieController.java
+++ Unit 3/src/test/java/com/javaunit3/springmvc/MovieController.java
@@ -1,5 +1,7 @@
 package com.javaunit3.springmvc;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import org.hibernate.Session;
 
@@ -10,46 +12,49 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javaunit3.springmvc.model.MovieEntity;
+import com.javaunit3.springmvc.model.VoteEntity;
 
 @Controller
 public class MovieController {
     @Autowired
     private BestMovieService bestMovieService;
-    /* 
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @RequestMapping("/")
     public String getIndexPage()
     {
         return "index";
-    }*/
+    }
     
-    // @RequestMapping("/bestMovie")
-    // public String getBestMoviePage( Model model) {
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage( Model model) {
 
-    // model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
-    //     return "bestMovie";
-    // }
+    model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
     
-    /* 
     @RequestMapping("/voteForBestMovieForm")
     public String voteForBestMovieFormPage() {
     return "voteForTheBestMovie";
     }
 
     @RequestMapping("/voteForBestMovie")
-    public String voteForBestMovie(HttpServletRequest request, Model model) {
-
-        String movieTitle = request.getParameter("movieTitle");
-
-        // int new_movie = Integer.parseInt(movieTitle);
-        // new_movie++;
-        model.addAttribute("BestMovieVote", movieTitle);
-
-    return "voteForTheBestMovie";
+    public String voteForBestMovie(HttpServletRequest request, Model model){
+        String movieId = request.getParameter("movieId");
+        String voterName = request.getParameter("voterName");
+        
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        MovieEntity movieEntity = (MovieEntity) session.get(MovieEntity.class, Integer.parseInt(movieId));
+        
+        VoteEntity newVote = new VoteEntity();
+        newVote.setVoterName(voterName);
+        movieEntity.addVote(newVote);
+        session.update(movieEntity);
+        session.getTransaction().commit();
+        return "voteForBestMovie";
     }
-    */
-    ///* 
-    @Autowired
-    private SessionFactory sessionFactory;
 
     @RequestMapping("/addMovieForm")
     public String addMovieForm() {
@@ -57,7 +62,7 @@ public class MovieController {
     }
 
     @RequestMapping("/addMovie")
-    public String voteForBestMovie(HttpServletRequest request, Model model) {
+    public String addMovie(HttpServletRequest request, Model model) {
 
         String movieTitle = request.getParameter("movieTitle");
         String maturityRating = request.getParameter("maturityRating");
@@ -67,23 +72,28 @@ public class MovieController {
         model.addAttribute("addMaturityRating", maturityRating);
         model.addAttribute("addgenre", genre);
 
-        //new movientity object
-        //setters for fields
+        MovieEntity movieEntity = new MovieEntity();
 
-        MovieEntity movie1 = new MovieEntity();
-        movie1.setTitle(movieTitle);
-        movie1.setMaturityRating(maturityRating);
-        movie1.setGenre(genre);
+        movieEntity.setTitle(movieTitle);
+        movieEntity.setMaturityRating(maturityRating);
+        movieEntity.setGenre(genre);
        
         Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
-        System.out.println("--------Begin-------");
-        session.save(movie1);
-        System.out.println("--------Save-------");
+        session.save(movieEntity);
         session.getTransaction().commit();
-        System.out.println("--------Commit-------");
         
     return "addMovie";
     }
-    //*/
+
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage(Model model){ 
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<MovieEntity> movieEntityList = session.createQuery("from MovieEntity").list();
+        session.getTransaction().commit();
+        model.addAttribute("movies", movieEntityList);
+        return "voteForBestMovie";
+    }
+    
 }
